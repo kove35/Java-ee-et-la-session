79cbc66c636b00fc111e693483de39a06b388fe8
paterne <p.gankama@laposte.net>
2017-05-08 15:36:04 +0200

    Test commint2

--- src/web/ControleurServlet.java
+++ src/web/ControleurServlet.java
@@ -1,10 +1,8 @@
 package web;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,59 +10,59 @@ import javax.servlet.http.HttpSession;
 
 import metier.MetierJeu;
 
+//Test commint
 //@WebServlet("/ControleurServlet")
 public class ControleurServlet extends HttpServlet {
-	
-      private MetierJeu metier;
-      private String gagnant;
-      @Override
-    public void init() throws ServletException {
-    	  metier=new MetierJeu();
-    	 metier.genererSecret();
-    }
-    
-   
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		request.getRequestDispatcher("vueJeu.jsp").forward(request, response);
-	}
 
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session =request.getSession();
-		ModelJeu model;
-		
-			if(session.getAttribute("model")==null){
-				model=new ModelJeu();
-				session.setAttribute("model", model);
-			}else{
-				model=(ModelJeu) session.getAttribute("model");
-			}
-			
-			if(request.getParameter("action").equals("Rejouer"))
-			{
-				metier.genererSecret();
-				metier.setFin(false);
-				model.setNombre(0);
-				model.getHistorique().clear();
-				
-			}else
-			{
-					
-				int nombre=Integer.parseInt(request.getParameter("nombreDevinez"));
-				model.setNombre(nombre);
-				String resultatMetier=metier.jouer(nombre);
-				if(resultatMetier.indexOf("Bravo")>=0){
-					gagnant=request.getRemoteAddr();
-				}
-				if(resultatMetier.indexOf("Le jeu est terminé")>=0){
-					resultatMetier=resultatMetier+" ,le gagnant est :"+gagnant;
-				}
-				
-				model.getHistorique().add(resultatMetier);
-			}
-		
-		request.getRequestDispatcher("vueJeu.jsp").forward(request, response);
-	}
+    private MetierJeu metier;
+    private String    gagnant;
+
+    @Override
+    public void init() throws ServletException {
+        metier = new MetierJeu();
+        metier.genererSecret();
+    }
+
+    protected void doGet( HttpServletRequest request, HttpServletResponse response )
+            throws ServletException, IOException {
+
+        request.getRequestDispatcher( "vueJeu.jsp" ).forward( request, response );
+    }
+
+    protected void doPost( HttpServletRequest request, HttpServletResponse response )
+            throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        ModelJeu model;
+
+        if ( session.getAttribute( "model" ) == null ) {
+            model = new ModelJeu();
+            session.setAttribute( "model", model );
+        } else {
+            model = (ModelJeu) session.getAttribute( "model" );
+        }
+
+        if ( request.getParameter( "action" ).equals( "Rejouer" ) ) {
+            metier.genererSecret();
+            metier.setFin( false );
+            model.setNombre( 0 );
+            model.getHistorique().clear();
+
+        } else {
+
+            int nombre = Integer.parseInt( request.getParameter( "nombreDevinez" ) );
+            model.setNombre( nombre );
+            String resultatMetier = metier.jouer( nombre );
+            if ( resultatMetier.indexOf( "Bravo" ) >= 0 ) {
+                gagnant = request.getRemoteAddr();
+            }
+            if ( resultatMetier.indexOf( "Le jeu est terminé" ) >= 0 ) {
+                resultatMetier = resultatMetier + " ,le gagnant est :" + gagnant;
+            }
+
+            model.getHistorique().add( resultatMetier );
+        }
+
+        request.getRequestDispatcher( "vueJeu.jsp" ).forward( request, response );
+    }
 
 }
